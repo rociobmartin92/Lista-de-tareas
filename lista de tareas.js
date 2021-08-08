d718910c9663d8fd618e62fc218e06507862620d
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2021-08-08 14:22:37 -0300

    Add files via upload

---
+++ lista de tareas.js
@@ -0,0 +1,90 @@
+
+// VARIABLES
+
+var boton = document.getElementById("boton");
+var caja = document.getElementById("caja");
+var lista = document.getElementById("lista");
+
+
+// FUNCIONES
+
+     // Función agregar tarea:
+
+var agregarTarea = function(){
+    
+    var texto = caja.value;
+    
+    
+     if (texto === "") {
+         caja.setAttribute("placeholder" , "Escriba una tarea por favor");
+         caja.className = "ColorPlaceholder";
+         return false;
+     }
+
+    var nuevoli = document.createElement("li");  // 
+
+ var contenido = document.createTextNode(texto);  // 
+
+ nuevoli.appendChild(contenido);   // 
+
+
+ lista.appendChild(nuevoli);  // 
+ 
+ nuevoli.className = "tareas";    
+
+ 
+// Eliminar Tarea  : 
+
+
+    nuevoli.addEventListener("click",eliminarTarea);
+
+
+
+}
+
+
+    // Comprobar caja vacía
+
+var comprobarCaja = function(){
+
+    texto = caja.value;
+
+    if( texto === "") {
+        
+    } else 
+   this.value ="";
+        
+   
+   }
+   
+
+      // Función liminar tarea
+
+var eliminarTarea = function(){
+
+
+    this.parentNode.removeChild(this);
+
+
+}
+
+      
+
+
+// EVENTOS
+
+// Agregar Tarea
+
+boton.addEventListener("click", agregarTarea);
+
+
+
+
+// Comprobar Caja
+
+caja.addEventListener("click", comprobarCaja);
+
+
+
+
+
