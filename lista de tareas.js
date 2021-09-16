c7c21e35ca1044a53f9364bb4488b84b7b93a0b5
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2021-09-16 16:05:26 -0300

    Update lista de tareas.js

--- lista de tareas.js
+++ lista de tareas.js
@@ -1,28 +1,64 @@
-"use strict";
 
 var boton = document.getElementById("boton");
 var caja = document.getElementById("caja");
-var lista = document.getElementById("lista"); // FUNCIONES
-// Función agregar tarea:
+var lista = document.getElementById("lista");
 
-var agregarTarea = function () {
-  var texto = caja.value;
 
-  if (texto === "") {
-    caja.setAttribute("placeholder", "Escriba una tarea por favor");
-    caja.className = "ColorPlaceholder";
-    return false;
-  }
+// FUNCIONES
 
-  var nuevoli = document.createElement("li"); // 
+     // Función agregar tarea:
 
-  var contenido = document.createTextNode(texto); // 
+var agregarTarea = function(){
+    
+    var texto = caja.value;
+ 
+     if (texto === "") {
+         caja.setAttribute("placeholder" , "Escriba una tarea por favor");
+         caja.className = "ColorPlaceholder";
+         return false;
+     }
+    var nuevoli = document.createElement("li");  // 
+ var contenido = document.createTextNode(texto);  // 
+ nuevoli.appendChild(contenido);   // 
+ lista.appendChild(nuevoli);  // 
+ nuevoli.className = "tareas";    
 
-  nuevoli.appendChild(contenido); // 
+ 
+// Eliminar Tarea  : 
+    nuevoli.addEventListener("click",eliminarTarea);
 
-  lista.appendChild(nuevoli); // 
+}
 
-  nuevoli.className = "tareas"; // Eliminar Tarea  : 
 
-  nuevoli.addEventListener("click", eliminarTarea);
-}; // Comprobar caja vacía
+    // Comprobar caja vacía
+
+var comprobarCaja = function(){
+    texto = caja.value;
+    if( texto === "") {       
+    } else 
+   this.value ="";  
+   }
+      // Función liminar tarea
+
+var eliminarTarea = function(){
+
+    this.parentNode.removeChild(this);
+
+}
+
+// EVENTOS
+
+// Agregar Tarea
+
+boton.addEventListener("click", agregarTarea);
+
+
+
+
+// Comprobar Caja
+
+caja.addEventListener("click", comprobarCaja);
+
+
+
+
