cd4664685b83779a3c629b825f94e1b6a7c4771e
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2021-09-16 16:04:12 -0300

    Update lista de tareas.js

--- lista de tareas.js
+++ lista de tareas.js
@@ -1,90 +1,28 @@
-
-// VARIABLES
+"use strict";
 
 var boton = document.getElementById("boton");
 var caja = document.getElementById("caja");
-var lista = document.getElementById("lista");
+var lista = document.getElementById("lista"); // FUNCIONES
+// Función agregar tarea:
 
+var agregarTarea = function () {
+  var texto = caja.value;
 
-// FUNCIONES
+  if (texto === "") {
+    caja.setAttribute("placeholder", "Escriba una tarea por favor");
+    caja.className = "ColorPlaceholder";
+    return false;
+  }
 
-     // Función agregar tarea:
+  var nuevoli = document.createElement("li"); // 
 
-var agregarTarea = function(){
-    
-    var texto = caja.value;
-    
-    
-     if (texto === "") {
-         caja.setAttribute("placeholder" , "Escriba una tarea por favor");
-         caja.className = "ColorPlaceholder";
-         return false;
-     }
+  var contenido = document.createTextNode(texto); // 
 
-    var nuevoli = document.createElement("li");  // 
+  nuevoli.appendChild(contenido); // 
 
- var contenido = document.createTextNode(texto);  // 
+  lista.appendChild(nuevoli); // 
 
- nuevoli.appendChild(contenido);   // 
+  nuevoli.className = "tareas"; // Eliminar Tarea  : 
 
-
- lista.appendChild(nuevoli);  // 
- 
- nuevoli.className = "tareas";    
-
- 
-// Eliminar Tarea  : 
-
-
-    nuevoli.addEventListener("click",eliminarTarea);
-
-
-
-}
-
-
-    // Comprobar caja vacía
-
-var comprobarCaja = function(){
-
-    texto = caja.value;
-
-    if( texto === "") {
-        
-    } else 
-   this.value ="";
-        
-   
-   }
-   
-
-      // Función liminar tarea
-
-var eliminarTarea = function(){
-
-
-    this.parentNode.removeChild(this);
-
-
-}
-
-      
-
-
-// EVENTOS
-
-// Agregar Tarea
-
-boton.addEventListener("click", agregarTarea);
-
-
-
-
-// Comprobar Caja
-
-caja.addEventListener("click", comprobarCaja);
-
-
-
-
-
+  nuevoli.addEventListener("click", eliminarTarea);
+}; // Comprobar caja vacía
